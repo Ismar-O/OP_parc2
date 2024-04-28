ee3e69976d1518aa17de2d94c51a47f9643a9ec1
Ismar Osmanovic <ismar.osmanovic04@gmail.com>
2024-04-28 22:45:07 +0200

    Zadaci 1 2 3 Ismar

--- Zadaci/Zadatak_1/main.c
+++ Zadaci/Zadatak_1/main.c
@@ -4,6 +4,17 @@
 
 int main()
 {
-    printf("Ismar\n");
+    printf("Program koji ucitava 10 brojeva i racuna zbir i sredju vrijednost\n");
+    printf("Unesite 10 brojeva:\n");
+    int broj;
+    long int zbir;
+    float srednja;
+    const int brojbrojeva = 10;
+    for(int i = 0; i<brojbrojeva; i++){
+        scanf("%d", &broj);
+        zbir+=broj;
+    }
+    srednja = (float)zbir / brojbrojeva;
+    printf("Zbir brojeva je %d, a njihova srednja vrijednost %.2f", zbir, srednja);
     return 0;
 }
--- Zadaci/Zadatak_2/main.c
+++ Zadaci/Zadatak_2/main.c
@@ -4,6 +4,19 @@
 
 int main()
 {
-    printf("Ismar\n");
+    printf("Program za tablicu mnozenja od 1 do n\n");
+    printf("Upisite n\n");
+    int n;
+    scanf("%d", &n);
+
+    for(int k = 1; k<=10; k++){
+        for(int i = 1; i<=n; i++){
+
+            printf(" %d x%3.d = %3.d,", i, k , i*k);
+
+        }
+        printf("\b \b\n");// \b escape sequence pomjera kursor lijevo i ispisuje prazno mjesto reko  zadnjeg zareza u redu
+    }
+
     return 0;
 }
