4f127e0cf1d1cac7e0d5c5b1052c15fde1fbc8c1
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-04-29 21:17:58 +0200

    Add files via upload
    Zadatak 6 Mirsad

---
+++ Zadaci/Zadatak_6/Zadatak6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+  int input, average, biggest = 0, sum = 0, i = 0;
+  do{
+  	printf("Unesite broj: ");
+  	scanf("%d", &input);
+  	if(input % 2 != 0){
+  	  sum += input;
+      if(input > biggest)
+        biggest = input;
+    i++;    
+	}
+  } while(input != 0);
+  if(sum > 0){
+    average = sum / i;
+    printf("Srednja vrijednost unesenih neparnih brojeva je: %d\n", average);
+    printf("Najveci uneseni neparni broj je %d\n", biggest);
+  }
+  else
+    puts("Nije unesen nijedan neparan broj");
+  return 0;
+}
