492733e9e69a60c204cf494ccf42ba3e8aae15ac
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-04-30 16:45:13 +0200

    Zadatak 2 Mirsad

---
+++ Zadaci/Zadatak_2_Mirsad/Zadatak2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+  int i, j, n;
+  puts("Unesite broj do kog ce se izvrsavati mnozenje:");
+  scanf("%d", &n);
+  for(i = 1; i <= n; i++){
+  	for(j = 1; j <= 8; j++){
+  	  printf("%dx%d = %d", i, j, i*j);	
+  	  //za sprjecavanje ispisa zareza na kraju
+  	  if(j != 8)
+  	    printf(", ");
+	}
+	putchar('\n');
+  }
+  return 0;
+}
