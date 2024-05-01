851973b77c41a1b96dc7407c9a93589b5b670dfa
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-05-02 00:16:52 +0200

    Zadatak 17 Mirsad

---
+++ Zadaci/Zadatak_17/Zadatak17.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void outputNumber(double, char);
+
+int main(){
+  double num;
+  char c;
+  puts("Unesite realan broj i karakter:");
+  scanf("%lf %c", &num, &c);
+  outputNumber(num, c);
+  return 0;
+}
+
+void outputNumber(double num, char c){
+  if(c >= 'a' && c <= 'z' || c >= 'A'  && 3c <= 'Z')
+    printf("%.2lf", num);
+  else
+    printf("%.1lf", num);
+}
