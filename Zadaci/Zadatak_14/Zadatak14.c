ff5cc988fdd41db01d85219df6ecc36fee3e969a
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-05-01 17:04:26 +0200

    Zadatak 14 Mirsad

---
+++ Zadaci/Zadatak_14/Zadatak14.c
@@ -0,0 +1,19 @@
+// Osnovi Programiranja , Parcijala 2 Vjezba2/Zadatak13 Mirsad Masic
+#include<stdio.h>
+
+int check(int, int, int);
+
+int main(){
+  int x, y, n;
+  puts("Unesite dva broja, zatim gornju granicu:");
+  scanf("%d %d %d", &x, &y, &n);
+  int result = check(x, y, n);
+  if(result)
+    puts("Brojevi se nalaze u odgovarajucim granicama.");
+  else
+    puts("Brojevi se ne nalaze u odgovarajucim granicama.");
+}
+
+int check(int x, int y, int n){
+  return x >= 0 && x <= n-1 && y >= 0 && y <= n-1;
+}
