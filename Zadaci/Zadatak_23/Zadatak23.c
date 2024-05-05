6a101d6b74ddbac376e0b2d05762827fe0fc3659
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-05-05 15:45:36 +0200

    Zadatak 23 Mirsad

--- Zadaci/Zadatak_23/Zadatak23.c
+++ Zadaci/Zadatak_23/Zadatak23.c
@@ -1,19 +1,20 @@
 // Osnovi Programiranja , Parcijala 2 Vjezba2/Zadatak23 Mirsad Masic
 #include<stdio.h>
 
-void swapValue(int, int);
+void swapValue(int*, int*);
 
 int main(){
   int a, b;
   printf("Unesite broj a, zatim broj b: ");
   scanf("%d %d", &a, &b);
-  swapValue(a, b);
+  printf("Vrijednost broja a prije zamjene je: %d\nVrijednost broja b prije zamjene je: %d\n", a, b);
+  swapValue(&a, &b);
+  printf("Vrijednost broja a nakon zamjene je: %d\nVrijednost broja b nakon zamjene je: %d\n", a, b);
   return 0;
 }
 
-void swapValue(int x, int y){
-  int temp = x;
-  x = y;
-  y = temp;
-  printf("Vrijednost broja a nakon zamjene je: %d\nVrijednost broja b nakon zamjene je: %d\n", x, y);
+void swapValue(int *x, int *y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
 }
