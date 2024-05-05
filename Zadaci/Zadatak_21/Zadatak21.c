7c3a38805885952591bfadb5aace02bd9d2c14ab
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2024-05-05 15:42:58 +0200

    Zadatak 21 Mirsad

--- Zadaci/Zadatak_21/Zadatak21.c
+++ Zadaci/Zadatak_21/Zadatak21.c
@@ -1,20 +1,27 @@
 // Osnovi Programiranja , Parcijala 2 Vjezba2/Zadatak21 Mirsad Masic
 #include<stdio.h>
 
+void outputFib(int);
 int recursiveFunction(int);
 
 int main(){
   int n;
-  printf("Unesite sumu n clanova Fibonacijevog niza: ");
+  printf("Broj clanova Fibonacijevog niza: ");
   scanf("%d", &n);
-  int result = recursiveFunction(n);
-  printf("Suma %d clanova Fibonacijevog niza je: %d", n, result);
+  outputFib(n);
   return 0;
 }
 
-int recursiveFunction(int n){
-  if(n==1 || n==2)
+void outputFib(int n){
+  int i;
+  for(i = 0; i < n; i++){
+  	printf("%d. clan Fibonacijevog niza je: %d\n", i+1, recursiveFunction(i+1));
+  }
+}
+
+int recursiveFunction(int x){
+  if(x==1 || x==2)
     return 1;
   else
-    return recursiveFunction(n-1) + recursiveFunction(n-2);
+    return recursiveFunction(x-1) + recursiveFunction(x-2);
 }
